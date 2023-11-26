bd633aa54ba529ed0ec29baa3e9461dad1aef84f
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-11-26 15:04:15 -0800

    updated

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# 100-puppet_ssh_config.pp
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/sshd_config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+}
