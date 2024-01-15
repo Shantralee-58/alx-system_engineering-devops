cb177c780303842ee6ec1f5b3c736abce61d6cba
LindiweKhumalo <86086701+Shantralee-58@users.noreply.github.com>
2024-01-16 01:09:11 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Puppet manifest to install Flask package using pip3
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# File: 2-execute_a_command.pp
-
-exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
-  path    => ['/bin', '/usr/bin'],
-}
