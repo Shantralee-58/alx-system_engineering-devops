5101ace925845f893875bbcadc152b219a6579b9
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-23 14:10:43 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,36 @@
-class nginx {
-    package { 'nginx':
-        ensure => installed,
-    }
-
-    service { 'nginx':
-        ensure  => running,
-        require => Package['nginx'],
-    }
-
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => 'Hello World!',
-        require => Package['nginx'],
-    }
-
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        source  => 'puppet:///modules/nginx/default',
-        require => Package['nginx'],
-    }
+#!/usr/bin/env bash
+# Configure server using puppet
+# defines a Puppet class called nginx_server that
+# encapsulates the configuration for the Nginx server.
+class nginx_server {
+package { 'nginx':
+ensure => installed,
 }
+# manages the Nginx service.
+service { 'nginx':
+ensure => running,
+enable => true,
+require => Package['nginx'],
+}
+# manages the Nginx configuration file located at /etc/nginx/sites-available/default.
+file { '/etc/nginx/sites-available/default':
+ensure => present,
+content => "
+server {
+listen 80 default_server;
+listen [::]:80 default_server;
+root /var/www/html;
+index index.html index.htm;
+location / {
+return 200 'Hello World!';
+}
+location /redirect_me {
+return 301 http://cuberule.com/;
+}
+}
+",
+notify => Service['nginx'],
+}
+}
+# includes the nginx_server class, ensuring that it gets applied.
+include nginx_server
